b5cd4d57430816da85763cffea5846154bee795d
insoo lee <is1224lee@g.ucla.edu>
2017-07-10 11:43:17 -0700

    Import intro exaple

---
+++ app/math.js
@@ -0,0 +1,21 @@
+export function add(x, y){
+    return x + y;
+}
+export function mulitply(x, y){
+    return x * y;
+}
+
+export function divide(x, y){
+    if(y===0){
+        return "Can't Divide Zero";
+    }
+    return x / y;
+}
+export function subtract(x, y){
+    return x - y;
+}
+
+export const num1 = 5;
+
+export const num2 = 10;
+export const zero = 0;
